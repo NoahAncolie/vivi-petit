83a1c2fbce0e74c266f8fd84e9d4c42fd17b936d
Adrien Benaceur <noahancolie@gmail.com>
2022-08-10 16:24:27 +0200

    Front page Final Theme

--- src/pages/Lawyer/index.jsx
+++ src/pages/Lawyer/index.jsx
@@ -1,6 +1,8 @@
 const Lawyer = () => {
     return (
-        <div></div>
+        <div className="fade-out bg-main vh-full">
+            <h1>Viviane PETIT<br/>Avocat<br/><br/>Cholet 49309</h1>
+        </div>
     )
 }
 
