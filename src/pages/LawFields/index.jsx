2220a1f1fb5d7c182d8349bdf0eb6ad1bdc4e0ee
Adrien Benaceur <noahancolie@gmail.com>
2022-09-01 10:45:51 +0200

    Expertises Responsive

--- src/pages/LawFields/index.jsx
+++ src/pages/LawFields/index.jsx
@@ -32,30 +32,40 @@ const LawFields = () => {
 
     return (
         <>
-            <div className="bg bg-light fade-out lg-padding-10 sm-padding-1 md-padding-1" id="page">
-                <div className="fixed flex align-center justify-center h-full width-full">
-                    <div className="expert-container justify-center">
-                        {!isText ?
-                            <img src={centerPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 expert-center-picture grow-h block" />
-                            :
-                            <div className="expert-overlay">
-                                <div className="expert-text">
-                                    {centerText}
+            <div className="sm-hidden-h md-hidden-h">
+                <div className="bg bg-light fade-out lg-padding-10 sm-padding-1 md-padding-1" id="page">
+                    <div className="fixed flex align-center justify-center h-full width-full">
+                        <div className="expert-container justify-center">
+                            {!isText ?
+                                <img src={centerPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 expert-center-picture grow-h block" />
+                                :
+                                <div className="expert-overlay">
+                                    <div className="expert-text">
+                                        {centerText}
+                                    </div>
                                 </div>
-                            </div>
-                        }
+                            }
+                        </div>
                     </div>
+                </div >
+                <div className="col-left bg-main sm-hidden-h md-hidden-h">
+                    <button className="expert-button text-light" onClick={() => { setCenterPicture(RealEstatePicture); setCenterText(RealEstate) }}><img src={RealEstatePicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit Immobilier</button>
+                    <button className="expert-button text-light" onClick={() => { setCenterPicture(BuildingPicture); setCenterText(Construction) }}><img src={BuildingPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit de la construction</button>
+                    <button className="expert-button text-light" onClick={() => { setCenterPicture(SignedPicture); setCenterText(Commercial) }}><img src={SignedPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit commercial</button>
                 </div>
-            </div >
-            <div className="col-left bg-main">
-                <button className="expert-button text-light" onClick={() => { setCenterPicture(RealEstatePicture); setCenterText(RealEstate) }}><img src={RealEstatePicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit Immobilier</button>
-                <button className="expert-button text-light" onClick={() => { setCenterPicture(BuildingPicture); setCenterText(Construction) }}><img src={BuildingPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit de la construction</button>
-                <button className="expert-button text-light" onClick={() => { setCenterPicture(SignedPicture); setCenterText(Commercial) }}><img src={SignedPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit commercial</button>
+                <div className="col-right bg-main sm-hidden-h md-hidden-h">
+                    <button className="expert-button text-light" onClick={() => { setCenterPicture(HammerPicture); setCenterText(Civil) }}><img src={HammerPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit des procédures civiles d’exécution</button>
+                    <button className="expert-button text-light" onClick={() => { setCenterPicture(FamilyPicture); setCenterText(Family) }}><img src={FamilyPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit de la famille</button>
+                    <button className="expert-button text-light" onClick={() => { setCenterPicture(FamilyPicture); setCenterText(Safe) }}><img src={PeopleFighting} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit des sûretés</button>
+                </div>
             </div>
-            <div className="col-right bg-main">
-                <button className="expert-button text-light" onClick={() => { setCenterPicture(HammerPicture); setCenterText(Civil) }}><img src={HammerPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit des procédures civiles d’exécution</button>
-                <button className="expert-button text-light" onClick={() => { setCenterPicture(FamilyPicture); setCenterText(Family) }}><img src={FamilyPicture} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit de la famille</button>
-                <button className="expert-button text-light" onClick={() => { setCenterPicture(FamilyPicture); setCenterText(Safe) }}><img src={PeopleFighting} alt="drawing-of-a-lawyer" id="center-picture" className="radius-5 block expert-button-img" />Droit des sûretés</button>
+            <div className="lg-hidden-h bg-light padding-1-2">
+                <div className="border-top border-blue"><RealEstate/></div>
+                <div className="border-top border-blue"><Construction/></div>
+                <div className="border-top border-blue"><Commercial/></div>
+                <div className="border-top border-blue"><Civil/></div>
+                <div className="border-top border-blue"><Family/></div>
+                <div className="border-top border-blue"><Safe/></div>
             </div>
         </>
 
